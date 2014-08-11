0721dc8ed55e5426bd4ea4d0936c35bc060c8ba8
Thomas Lee <thomlee@seas.upenn.edu>
2014-08-11 00:45:45 -0700

    added finished-training path

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ app.get("/volunteer/load", controller.load_volunteer);
 app.post("/volunteer/submit-volunteer", controller.create_volunteer);
 app.get("/volunteer/sign-up", controller.volunteer_signup_page);
 app.get("/volunteer/home", controller.volunteer_home);
+app.get("/volunteer/training", controller.volunteer_training);
+app.get("/volunteer/finished-training", controller.volunteer_finished_training);
 app.post("/review/create/:id", controller.create_review); // org_id here
 app.get("/review/edit/:id", controller.edit_review); // review id
 app.post("/review/save/:id", controller.save_review); // review id
