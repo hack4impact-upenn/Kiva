6e41375fa04efcee31c76b2c762a605ca86abf04
Alex Harelick <harelick@seas.upenn.edu>
2014-10-19 16:24:58 -0400

    changed application data to org data and fixed curly braces

--- controllers/controller.js
+++ controllers/controller.js
@@ -208,11 +208,12 @@ exports.edit_review = function(req, res) {
 
 //Volunteer Helper Functions
 
-exports.load_application_data = function(req, res) {
+exports.load_organization_data = function(req, res) {
 	console.log("loading application data");
 	request('https://api.myjson.com/bins/1a2tl', function (error, response, body) {
 	  	if (!error && response.statusCode == 200) {
 	    	res.json(body)
+	    }
 	});
 }
 
